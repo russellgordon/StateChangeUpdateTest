9aec26e376792617ea9ea1f5b28cdd5bc2f8e1c9
Russell Gordon <github@russellgordon.ca>
2020-12-26 13:40:25 -0500

    Initial Commit

---
+++ StateChangeUpdateTest/StateChangeUpdateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateChangeUpdateTestApp.swift
+//  StateChangeUpdateTest
+//
+//  Created by Russell Gordon on 2020-12-26.
+//
+
+import SwiftUI
+
+@main
+struct StateChangeUpdateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
